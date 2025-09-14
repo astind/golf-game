8a2d8c97c71632e1b6772562ff1f119891c5c378
Dillon <dillon@astin.dev>
2025-09-13 23:42:33 -0400

    golf ball scene and assets

---
+++ scripts/golf_ball.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+const ball_ground = preload("res://assests/Ball/Ball-Sprites_0000.png")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
